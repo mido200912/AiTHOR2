c83546d278d488b9d1b8bdb307faea0cc2b0c66f
mido200912 <mohammedalsyedalsyed12@gmail.com>
2025-10-17 09:03:45 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ export const requireAuth = async (req, res, next) => {
     const auth = req.headers.authorization;
     if (!auth?.startsWith("Bearer ")) return res.status(401).json({ error: "No token" });
     const token = auth.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET||"fa7b3bb0a8bb654007d89fd5209427bd202164b0287c775c760f627d464ee143142771e793bdd2b7f0e1b56bab872e09203ba11278abe36053a878ae51bb575f");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select("-password");
     if (!user) return res.status(401).json({ error: "Invalid token" });
     req.user = user;
